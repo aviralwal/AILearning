46209f46655e93ebce7adac4229f1c084894a11f
Aviral Agrawal <aviralagrawal260@gmail.com>
2026-08-03 21:50:04 +0530

    Added airline ai assistant and TODO

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # AILearning
+
+Example of AI Tool call - airlineaiassistant.py
+Gradio specific examples - present in `gradio_learning` folder
+
+
+# TODO 
+Clean up the file day5.ipynb
+Update the project to upyter notebook
--- airlineaiassistant.py
+++ airlineaiassistant.py
@@ -3,9 +3,9 @@ import json
 from dotenv import load_dotenv
 from openai import OpenAI
 import gradio as gr
+import sqlite3
 
 # Initialization
-
 load_dotenv(override=True)
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
@@ -24,21 +24,88 @@ Give short, courteous answers, no more than 1 sentence.
 Always be accurate. If you don't know the answer, say so.
 """
 
+#Creating a database tool to get and set ticket prices for use with the chat function
+DB = "prices.db"
+
+with sqlite3.connect(DB) as conn:
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS prices (city TEXT PRIMARY KEY, price REAL)')
+    conn.commit()
+
+def get_ticket_price_from_database(city):
+    print(f"DATABASE TOOL CALLED: Getting price for {city}", flush=True)
+    with sqlite3.connect(DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT price FROM prices WHERE city = ?', (city.lower(),))
+        result = cursor.fetchone()
+        return f"Ticket price to {city} is ${result[0]}" if result else "No price data available for this city"
+
+print(get_ticket_price_from_database("London"))
+
+def set_ticket_price_in_database(city, price):
+    with sqlite3.connect(DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO prices (city, price) VALUES (?, ?) ON CONFLICT(city) DO UPDATE SET price = ?', (city.lower(), price, price))
+        conn.commit()
+
+# Populating the database with some dummy data
+ticket_prices = {"london":799, "paris": 899, "tokyo": 1420, "sydney": 2999}
+for city, price in ticket_prices.items():
+    set_ticket_price_in_database(city, price)
+
+print(get_ticket_price_from_database("Tokyo"))
+
+# Defining the tool function that can be called by the LLM
+
+price_function = {
+    "name": "get_ticket_price_from_database",
+    "description": "Get the price of a return ticket to the destination city.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "destination_city": {
+                "type": "string",
+                "description": "The city that the customer wants to travel to",
+            },
+        },
+        "required": ["destination_city"],
+        "additionalProperties": False
+    }
+}
+
+tools = [{"type": "function", "function": price_function}]
+
+# We have to write that function handle_tool_calls to handle any tool calls needed by LLM retruned in response
+def handle_tool_calls(message):
+    responses = []
+    for tool_call in message.tool_calls:
+        if tool_call.function.name == "get_ticket_price_from_database":
+            arguments = json.loads(tool_call.function.arguments)
+            city = arguments.get('destination_city')
+            price_details = get_ticket_price_from_database(city)
+            responses.append({
+                "role": "tool",
+                "content": price_details,
+                "tool_call_id": tool_call.id
+            })
+    return responses
+
+# Updating chat function to allow tool calls
 def chat(message, history):
     history = [{"role":h["role"], "content":h["content"]} for h in history]
     messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
-    response = openai.chat.completions.create(model=MODEL, messages=messages)
+    response = openai.chat.completions.create(model=MODEL, messages=messages, tools=tools)
+
+    while response.choices[0].finish_reason=="tool_calls": # Since there can be multiple tool calls in a single chat response, hence while loop to keep on calling handle tool functions until all tool calls are done
+        message = response.choices[0].message
+        responses = handle_tool_calls(message) # Function to call tool based on response from LLM
+        messages.append(message)
+        messages.extend(responses)
+        response = openai.chat.completions.create(model=MODEL, messages=messages, tools=tools)
+    
     return response.choices[0].message.content
 
-# Creating a simple chat bot for airline tickets
-#gr.ChatInterface(fn=chat, type="messages").launch()
+# We can now use the database tool in the chat function
+gr.ChatInterface(fn=chat, type="messages").launch()
 
-# An example of tool - Creating a function to get ticket prices 
-ticket_prices = {"london": "$799", "paris": "$899", "tokyo": "$1400", "berlin": "$499"}
 
-def get_ticket_price(destination_city):
-    print(f"Tool called for city {destination_city}")
-    price = ticket_prices.get(destination_city.lower(), "Unknown ticket price")
-    return f"The price of a ticket to {destination_city} is {price}"
-
-print(get_ticket_price("London"))
